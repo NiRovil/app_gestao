7595c939b13c275233ed68503b03af5cd9dc37bd
NiRovil <nicolas199851@hotmail.com>
2023-06-12 16:34:43 -0300

    testando algumas funcoes de db

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Fornecedor;
 
 class ContatoController extends Controller
 {
     public function contato(){
 
         $titulo = ['titulo' => 'Contato'];
-        var_dump($_POST);
-        return view('site.contato', compact('titulo'));
+        $fornecedores = Fornecedor::all();
+        var_dump($fornecedores);
+        //var_dump($_POST);
+        return view('site.contato', compact('titulo', 'fornecedores'));
     }
 }
