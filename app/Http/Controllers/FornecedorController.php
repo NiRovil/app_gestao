6aef5d7becac036a76fccfa9613b88e681753d8d
NiRovil <nicolas199851@hotmail.com>
2023-06-26 18:54:50 -0300

    implementando validacao de fornecedores e o respectivo cadastro no banco de dados

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Fornecedor;
 use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
@@ -19,7 +20,38 @@ class FornecedorController extends Controller
         return view('app.fornecedor.listar', compact('titulo'));
     }
 
-    public function adicionar(){
+    public function adicionar(Request $request){
+
+        if($request->input('_token') != ''){
+            $regras = [
+                'nome' => 'required',
+                'UF' => 'required|min:2|max:2',
+                'email' => 'required|email'
+            ];
+
+            $feedback = [
+                'required' => 'O campo :attribute deve ser preenchido.',
+                'UF.min' => 'O campo UF deve conter somente 2 caracteres.',
+                'UF.max' => 'O campo UF deve conter somente 2 caracteres.',
+                'email' => 'Insira um email válido.'
+            ];
+
+            $request->validate($regras, $feedback);
+
+            $fornecedor = new Fornecedor();
+            $fornecedor->create($request->all());
+
+            /* Salvando fornecedor com base em cada atributo.
+                $fornecedor = new Fornecedor();
+                $fornecedor->nome = $request->input('nome');
+                $fornecedor->UF = $request->input('uf');
+                $fornecedor->email = $request->input('email');
+
+                $fornecedor->save();
+            */
+        }
+
+
         $titulo = ['Adicionar'];
         return view('app.fornecedor.adicionar', compact('titulo'));
     }
--- resources/views/app/fornecedor/adicionar.blade.php
+++ resources/views/app/fornecedor/adicionar.blade.php
@@ -12,10 +12,17 @@
 
         <div class="informacao-pagina">
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                <form method="POST" action="">
-                    <input type="text" name="nome" placeholder="Nome" class="borda-preta">
-                    <input type="text" name="uf" placeholder="UF" class="borda-preta">
-                    <input type="text" name="email" placeholder="Email" class="borda-preta">
+                <form method="POST" action={{route('app.fornecedor.adicionar')}}>
+                    @csrf
+                    <input type="text" name="nome" value="{{old('nome')}}" placeholder="Nome" class="borda-preta">
+                    {{$errors->has('nome') ? $errors->first('nome') : ''}}
+
+                    <input type="text" name="UF" value="{{old('UF')}}" placeholder="UF" class="borda-preta">
+                    {{$errors->has('UF') ? $errors->first('UF') : ''}}
+
+                    <input type="text" name="email" value="{{old('email')}}" placeholder="Email" class="borda-preta">
+                    {{$errors->has('email') ? $errors->first('email') : ''}}
+
                     <button type="submit" class="borda-preta">Cadastrar</button>
                 </form>
             </div>
